f046e041d6fa68ff6fb62602252c87d185fdc367
Mario Brusarosco <mariobrusarosco@hotmail.com>
2024-04-29 14:20:01 -0300

    fix: removing files with solely testing purposes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-};
+const nextConfig = {};
 
-const withMDX = createMDX({});
-
-export default withMDX(nextConfig);
+export default nextConfig;
